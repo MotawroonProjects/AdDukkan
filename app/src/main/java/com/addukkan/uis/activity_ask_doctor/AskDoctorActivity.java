cdb6102b1285cac59fb96c81157c337a499064cc
EL MOTWRON <ahmedmohamed23345@gmail.com>
2021-04-14 15:31:51 +0200

    ch

---
+++ app/src/main/java/com/addukkan/uis/activity_ask_doctor/AskDoctorActivity.java
@@ -0,0 +1,117 @@
+package com.addukkan.uis.activity_ask_doctor;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.addukkan.R;
+import com.addukkan.adapters.DoctorsAdapter;
+import com.addukkan.databinding.ActivityAskDoctorBinding;
+import com.addukkan.interfaces.Listeners;
+import com.addukkan.language.Language;
+
+import io.paperdb.Paper;
+
+public class AskDoctorActivity extends AppCompatActivity implements Listeners.BackListener {
+    private ActivityAskDoctorBinding binding;
+private String lang;
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        Paper.init(newBase);
+        super.attachBaseContext(Language.updateResources(newBase, Paper.book().read("lang", "ar")));
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_ask_doctor);
+        initView();
+
+    }
+
+    private void initView() {
+        Paper.init(this);
+        lang=Paper.book().read("lang","ar");
+        binding.setLang(lang);
+        binding.setBackListener(this);
+        binding.llSpecialization.setOnClickListener(v -> openSheet());
+        binding.imageCloseSpecialization.setOnClickListener(v -> closeSheet());
+        binding.progBar.setVisibility(View.GONE);
+        binding.recView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recView.setAdapter(new DoctorsAdapter(this));
+
+    }
+
+    private void openSheet() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+
+        binding.flSpecializationSheet.clearAnimation();
+        binding.flSpecializationSheet.startAnimation(animation);
+
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.flSpecializationSheet.setVisibility(View.VISIBLE);
+
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+    private void closeSheet() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+
+        binding.flSpecializationSheet.clearAnimation();
+        binding.flSpecializationSheet.startAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.flSpecializationSheet.setVisibility(View.GONE);
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void back() {
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        back();
+    }
+}
---
+++ app/src/main/java/com/addukkan/uis/activity_doctor_detials/DoctorDetialsActivity.java
@@ -0,0 +1,56 @@
+package com.addukkan.uis.activity_doctor_detials;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.addukkan.R;
+import com.addukkan.adapters.DoctorsAdapter;
+import com.addukkan.databinding.ActivityDoctorDetialsBinding;
+import com.addukkan.interfaces.Listeners;
+import com.addukkan.language.Language;
+
+import io.paperdb.Paper;
+
+public class DoctorDetialsActivity extends AppCompatActivity implements Listeners.BackListener {
+    private ActivityDoctorDetialsBinding binding;
+    private String lang;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        Paper.init(newBase);
+        super.attachBaseContext(Language.updateResources(newBase, Paper.book().read("lang", "ar")));
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_doctor_detials);
+        initView();
+
+    }
+    private void initView() {
+        Paper.init(this);
+        lang=Paper.book().read("lang","ar");
+        binding.setLang(lang);
+        binding.setBackListener(this);
+
+
+    }
+
+    @Override
+    public void back() {
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        back();
+    }
+}
